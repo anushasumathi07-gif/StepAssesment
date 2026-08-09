30f7eba746eea9e6a7f4804ac2cec4462e4b8d6b
anushasumathi07-gif <anushasumathi07@gmail.com>
2026-08-09 11:39:17 +0530

    UC1 added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
 
-public class Main {
+    import java.util.Scanner;
+    public class Main {
+        static void checkPinLength(String pin) {
+            if (pin.length() != 4) {
+                System.out.println("Invalid PIN — must be exactly 4 digits.");
+            } else {
+                System.out.println("PIN length OK.");
+            }
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter PIN: ");
+            String pin = sc.nextLine();
+
+            checkPinLength(pin);
+        }
+
 }
